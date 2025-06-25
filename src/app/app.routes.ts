b317e7391a849ac6e7652a2a9d315c2067847608
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-06-25 08:11:14 -0500

    subiendo nuevos componentes, y configurando el componente global

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { List } from './Domains/Products/Pages/list/list'
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: List
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule ,RouterOutlet],
+  template: '<router-outlet />'
 })
 export class App {
   protected title = 'Store';
